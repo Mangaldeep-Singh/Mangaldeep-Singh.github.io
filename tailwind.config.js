d12774271318ee5f478e1b6bba625027a936483a
MANGALDEEP SINGH <49555622+Mangaldeep-Singh@users.noreply.github.com>
2024-07-20 00:05:42 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 module.exports = {
-  purge: [
+  content: [
     './_includes/**/*.html',
     './_layouts/**/*.html',
     './_pages/**/*.html',
     './_posts/**/*.md',
     './*.html',
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
